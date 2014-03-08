bc668a8a00ca47e218dec510bca73782cd7d0c2c
Huayi Zhang <irachex@gmail.com>
2014-03-08 17:26:18 +0800

    itertool rocks

---
+++ letter_combinations_of_a_phone_number.py
@@ -0,0 +1,46 @@
+#coding: utf-8
+
+'''
+http://oj.leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+Given a digit string, return all possible letter combinations that the number could represent.
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+'''
+
+MAPPING = {
+    '1': '',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+    '0': ' ',
+}
+
+class Solution:
+    # @return a list of strings, [s1, s2]
+    def letterCombinations(self, digits):
+        if not digits:
+            return [""]
+
+        # copy from itertools.product
+        def product(*args):
+            pools = map(tuple, args)
+            result = [[]]
+            for pool in pools:
+                result = [x + [y] for x in result for y in pool]
+            for prod in result:
+                yield tuple(prod)
+
+        return map(lambda x: ''.join(x), product(*map(MAPPING.get, digits)))
+
+
+if __name__ == "__main__":
+    print Solution().letterCombinations('23')
