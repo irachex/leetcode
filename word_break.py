c11db3ed5d7fbc3178a0ef7f6666d6f971641175
Huayi Zhang <irachex@gmail.com>
2014-03-09 00:27:08 +0800

    simple dp

---
+++ word_break.py
@@ -0,0 +1,32 @@
+#coding: utf-8
+
+'''
+http://oj.leetcode.com/problems/word-break/
+
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+For example, givens = "leetcode",dict = ["leet", "code"].
+Return true because "leetcode" can be segmented as "leet code".
+'''
+
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a boolean
+    def wordBreak(self, s, dict):
+        if not dict:
+            return False
+        words = set(dict)
+        max_len = max(len(w) for w in words)
+        n = len(s)
+        d = [False for i in range(n + 1)]
+        d[0] = True
+        for i in range(1, n + 1):
+            for j in range(1, min(i, max_len) + 1):
+                if d[i - j] and s[i - j:i] in words:
+                    d[i] = True
+                    break
+        return d[n]
+
+
+if __name__ == "__main__":
+    assert Solution().wordBreak("leetcode", ['leet', 'code'])
