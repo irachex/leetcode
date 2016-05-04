84616a61e9846b480734cb50d2e06ae8e857df17
Huayi Zhang <irachex@gmail.com>
2016-05-04 21:11:45 +0800

    Add power-of-three.py

---
+++ power-of-three.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/power-of-three/
+
+Given an integer, write a function to determine if it is a power of three.
+
+Follow up:
+Could you do it without using any loop / recursion?
+'''
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        while n > 1:
+            if n % 3 != 0:
+                return False
+            n /= 3
+        return n == 1
+
