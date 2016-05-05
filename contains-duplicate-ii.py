7581c9e45490cac3e84bd565040f00b334158ab9
Huayi Zhang <irachex@gmail.com>
2016-05-05 23:06:17 +0800

    Add contains-duplicate-ii.py

---
+++ contains-duplicate-ii.py
@@ -0,0 +1,20 @@
+'''
+https://leetcode.com/problems/contains-duplicate-ii/
+
+Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+'''
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        d = {}
+        for i, x in enumerate(nums):
+            last = d.get(x)
+            if last is not None and i - last <= k:
+                return True
+            d[x] = i
+        return False
