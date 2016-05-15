542fdad9ca353c3bcc637092bde75002f7138bc9
Huayi Zhang <irachex@gmail.com>
2016-05-15 16:24:54 +0800

    Add reconstruct-itinerary.py

---
+++ reconstruct-itinerary.py
@@ -0,0 +1,46 @@
+'''
+https://leetcode.com/problems/reconstruct-itinerary/
+
+Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
+
+Note:
+If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string. For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+All airports are represented by three capital letters (IATA code).
+You may assume all tickets form at least one valid itinerary.
+Example 1:
+tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+Return ["JFK", "MUC", "LHR", "SFO", "SJC"].
+Example 2:
+tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Return ["JFK","ATL","JFK","SFO","ATL","SFO"].
+Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"]. But it is larger in lexical order.
+'''
+
+
+from collections import defaultdict
+
+
+class Solution(object):
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        edges = defaultdict(list)
+        for u, v in sorted(tickets, reverse=True):
+            edges[u].append(v)
+
+        def dfs(p):
+            while edges[p]:
+                dfs(edges[p].pop())
+            path.append(p)
+
+        path = []
+        dfs('JFK')
+        return path[::-1]
+
+
+if __name__ == '__main__':
+    f = Solution().findItinerary
+    assert f([["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]) == ["JFK", "MUC", "LHR", "SFO", "SJC"]
+    assert f([["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]) == ["JFK","ATL","JFK","SFO","ATL","SFO"]
