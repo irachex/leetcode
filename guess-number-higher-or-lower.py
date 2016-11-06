1e0617704d98057c81048a602ce1a17ee0c63fa6
Huayi Zhang <irachex@gmail.com>
2016-11-06 23:51:11 +0800

    Add guess-number-higher-or-lower.py

---
+++ guess-number-higher-or-lower.py
@@ -0,0 +1,42 @@
+'''
+https://leetcode.com/problems/guess-number-higher-or-lower/
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example:
+n = 10, I pick 6.
+
+Return 6.
+'''
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lower, upper = 1, n
+        while lower <= upper:
+            mid = (lower + upper) / 2
+            r = guess(mid)
+            if r == 0:
+                return mid
+            elif r == -1:
+                upper = mid - 1
+            elif r == 1:
+                lower = mid + 1
