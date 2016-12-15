272999106d6cd1721f3ca09ed353a4a6237ea23a
Huayi Zhang <irachex@gmail.com>
2016-12-15 22:33:18 +0800

    🌜

--- largest-rectangle-in-histogram.py
+++ largest-rectangle-in-histogram.py
@@ -3,7 +3,10 @@
 '''
 http://oj.leetcode.com/problems/largest-rectangle-in-histogram/
 
-Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.\n\nAbove is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].\n\nThe largest rectangle is shown in the shaded area, which has area = 10 unit.\n\nFor example,
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+The largest rectangle is shown in the shaded area, which has area = 10 unit.
+For example,
 Given height = [2,1,5,6,2,3],
 return 10.
 '''
